5e8398db56c0c4ef4238d6fefbd4915cfc5e225d
Mario343214420 <343214420@qq.com>
2018-12-03 21:14:48 +0800

    服务器初步搭建完成

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 /**
  * Created by Administrator on 2018/12/3.
  */
+const express = require('express');
+const app = express();
+const router = require('./router');
+const db = require('./db');
+(async () => {
+  await db;
+  app.use(router);
+});
+
+app.listen(4000, err => {
+  if(!err) console.log(('服务器启动完成(请访问-   http://localhost:4000 )'));
+  else console.log(err);
+})
