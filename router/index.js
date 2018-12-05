59200c8c4e5de8a7520918c24a8dbb9f9a10733a
Mario343214420 <343214420@qq.com>
2018-12-05 08:09:29 +0800

    服务器路由注册完成，登录完成,测试成功

--- router/index.js
+++ router/index.js
@@ -41,6 +41,39 @@ router.post('/register', async (req, res) => {
     })
   }
 })
-
+router.post('/login', async (req, res) => {
+  //获取用户提交请求参数信息
+  const {username, password} = req.body;
+  console.log(username, password);
+  
+  try {
+    //去数据库查找当前用户是否存在
+    const user = await Users.findOne({username, password: md5(password)});
+    if (user) {
+      //用户可登录，登录成功
+      res.json({
+        code: 0,
+        data: {
+          _id: user.id,
+          type: user.type,
+          username: user.username
+        }
+      })
+    } else {
+      //用户名或密码错误
+      //返回失败的响应
+      res.json({
+        code: 1,
+        msg: '用户名或密码错误'
+      })
+    }
+  } catch (e) {
+    console.log(e);
+    res.json({
+      code: 2,
+      msg: '网络不稳定，请刷新试试~'
+    })
+  }
+})
 
 module.exports = router;
